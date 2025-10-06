0316c4c566b0ce66d11299ea25217d3168aa9212
basaraksu <basaraksu03@gmail.com>
2025-10-06 23:04:38 +0300

    add a login route and jwt middleware

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,5 +1,9 @@
 import User from "../models/userModel.js"
+import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+import { configDotenv } from "dotenv";
 
+configDotenv()
 
 export const registerUser = async (req, res) => {
   try {
@@ -25,6 +29,40 @@ export const registerUser = async (req, res) => {
     
     // For now, just echo back the data
     return res.status(200).json({ message: 'Registered', data: req.body });
+
+  } catch (error) {
+    return res.status(500).json({ error: 'Something went wrong', details: error.message });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+      return res.status(400).json({ error: 'Both username and password are required' });
+    }
+
+    const user = await User.findOne({ username }); 
+
+    if (!user) {
+      return res.status(401).json({ error: 'No user exists. Please register first.' });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password); // compare hashed password
+    if (!isMatch) {
+      return res.status(401).json({ error: 'Incorrect password' });
+    }
+
+    //  User is authenticated
+
+    const token = jwt.sign(
+      { id: user._id, username: user.username },
+      process.env.JWT_TOKEN,
+      { expiresIn: '1h' }
+    );
+
+    return res.status(200).json({ message: 'Login successful', token });
 
   } catch (error) {
     return res.status(500).json({ error: 'Something went wrong', details: error.message });
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,20 @@
 import express from "express"
-import { registerUser } from "../controllers/authControllers.js"
+import { loginUser, registerUser } from "../controllers/authControllers.js"
 
 const router = express.Router()
 
 router.post("/register", registerUser)
+
+router.post("/login", loginUser)
+
+
+router.get('/login', (req, res) => {
+  res.status(200).send('Login route is active. Use POST to log in.');
+});
+
+router.get('/register', (req, res) => {
+  res.status(200).send('Register route is active. Use POST to register.');
+});
+
 
 export default router
